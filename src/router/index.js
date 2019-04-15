91215d005444fbc6396730b794668991046fa681
zhao-dong <zhaolinpu@gmail.com>
2019-04-15 18:43:02 +0800

    added missing file

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import About from '@/components/About'
+import News from '@/components/News'
 
 Vue.use(Router)
 
@@ -10,6 +12,16 @@ export default new Router({
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
+    },
+    {
+        path: '/about',
+        name: 'About',
+        component: About
+    },
+    {
+        path: '/news',
+        name: 'News',
+        component: News
     }
   ]
 })
